58f73a56d6a987cf96e731374febc2e4f4d5cbcb
ericghost <ericghost@users.noreply.github.com>
2020-04-04 13:43:04 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @ECHO OFF 
 %1 start mshta vbscript:createobject("wscript.shell").run("""%~0"" ::",0)(window.close)&&exit
 
-start /b speederv2.exe -c -l0.0.0.0:9898 -ryour_server_ip:udpspeeder_server_port -k "ericvm" --mode 0 -f1:3,2:4,8:6,20:10 -q1 
+start /b speederv2.exe -c -l0.0.0.0:9898 -ryour_server_ip:udpspeeder_server_port -k "ericvm" --mode 0 -f2:4,20:10 -q1 
 start /b client_windows_amd64.exe -c kcptun_client.json 
